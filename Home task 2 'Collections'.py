5329bd59f3b93eb259028b1ad1724e39daf2ef80
MykhailoZaporozhchenko <98468840+MykhailoZaporozhchenko@users.noreply.github.com>
2022-02-02 12:28:50 +0200

    Create Home task 2 'Collections'.py

---
+++ Home task 2 'Collections'.py
@@ -0,0 +1,53 @@
+import string
+import random
+
+
+# Initionalithation random list of dictionaries (2 - 10 dictionaries; 1 - 26 keys as ascii_lowercase, values as 0 -100)
+dictionaries_list = [] 
+for numder in range(0, random.randint(2, 10)):
+    n_dict = {}
+    key_list = random.sample(string.ascii_lowercase, random.randint(1, 26))  
+    for n_key in key_list:
+        n_dict[n_key] = random.randint(0, 100)
+    dictionaries_list.append(n_dict)
+
+print(dictionaries_list)   # dictionaries_list test.
+
+#Initionalithation a dictionary showing the quantity for each key.
+matrix = {}
+for dict in dictionaries_list:
+    for key in dict.keys():
+        if key not in matrix.keys():
+            matrix[key] = 1
+        else:
+            matrix[key] += 1
+
+print(matrix) # matrix test
+
+# Initionalithation a dictionary filled with MAX value for each key.
+result = {}
+for  dict in dictionaries_list:
+    for key in dict.keys():
+        if matrix[key] == 1:
+            result[key] = dict[key]
+        else:
+            if key not in result.keys():
+                result[key] = dict[key]
+            else:
+                if result[key] < dict[key]:
+                    result[key] = dict[key]
+
+print(result) #result (no index) test.
+
+
+# Indexing non-unique keys
+for id, dict in enumerate(dictionaries_list):
+    for key in dict.keys():
+        if matrix[key] != 1:
+            if key in result.keys():
+                if result[key] == dict[key]:
+                    result.pop(key)
+                    result[str(key + '_' + str(id + 1))] = dict[key]
+            
+        
+print(result);
